feeb637b7bec264cb07213ee8baa6f2fbbad7994
Paul James <mail@paulrichardjames.net>
2024-05-26 14:22:20 +0100

    removed print statements

--- process_data_with_pandas.py
+++ process_data_with_pandas.py
@@ -15,12 +15,8 @@ data = pd.read_csv(file_path, dtype=column_types)
 
 data['Date of Birth as Date'] = pd.to_datetime(data['Date of Birth'], errors='coerce')
 
-print(data)
-
 invalid_dates = data[data['Date of Birth as Date'].isna()].copy()
 valid_dates =  data[data['Date of Birth as Date'].notna()].copy()
-
-print(invalid_dates)
 
 valid_dates['Age in Years'] = valid_dates['Date of Birth as Date'].apply(lambda x: relativedelta(datetime.now(), x).years)
 
@@ -30,7 +26,6 @@ invalid_dates[['First Name', 'Family Name', 'Date of Birth']].to_csv('Output/inv
 
 print('Data processing complete')
 print('Check the Output folder for the results')
-
-print(f"Number of rows  processed: {data.shape[0]}")
+print(f"Number of rows processed: {data.shape[0]}")
 print(f"Number of rows with valid dates: {valid_dates.shape[0]}")
 print(f"Number of rows with invalid dates: {invalid_dates.shape[0]}")
